f8ffb6bd7708ff43b3c9b45fe4309b010abcee94
Michael Rittmeister <dr@schlau.bi>
2023-07-30 13:14:51 +0200

    Add Kotlin examples

---
+++ kotlin/MariaDB.kt
@@ -0,0 +1,45 @@
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import kotlin.system.exitProcess
+
+const val driver = "com.mariadb.jdbc.Driver"
+
+class MariaDB(
+    hostname: String,
+    database: String,
+    port: String,
+    private val username: String,
+    private val password: String
+) {
+    private val url: String = "jdbc:mysql://$hostname:$port/$database"
+    private lateinit var conn: Connection
+
+    @kotlin.Throws(ClassNotFoundException::class, SQLException::class)
+    fun connect(): Connection {
+        Class.forName(driver)
+        conn = DriverManager.getConnection(url, username, password)
+        return conn
+    }
+}
+
+fun main(args: Array<String>) {
+    val hostname = "<$hostname>"
+    val database = "<$database>"
+    val port = "<$port>"
+    val username = "<$user>"
+    val password = "<$password>"
+    val mysql = MariaDB(hostname, database, port, username, password)
+    mysql.connect().use { conn ->
+        println(String.format("Conected: %b", !conn.isClosed))
+        if (conn.isClosed) {
+            println("Terminating as the connection is closed.")
+            exitProcess(1)
+        }
+        conn.createStatement().executeQuery("SELECT 1+1").use {resultSet ->
+            if (resultSet.next()) {
+                println("Result of query ${resultSet.getInt("1+1")}: %s")
+            }
+        }
+    }
+}
---
+++ kotlin/MySQL.kt
@@ -0,0 +1,44 @@
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import kotlin.system.exitProcess
+
+const val driver = "com.mysql.jdbc.Driver"
+
+class MySQL(
+    hostname: String,
+    database: String,
+    port: String,
+    private val username: String,
+    private val password: String
+) {
+    private val url: String = "jdbc:mysql://$hostname:$port/$database"
+    private lateinit var conn: Connection
+
+    fun connect(): Connection {
+        Class.forName(driver)
+        conn = DriverManager.getConnection(url, username, password)
+        return conn
+    }
+}
+
+fun main(args: Array<String>) {
+    val hostname = "<$hostname>"
+    val database = "<$database>"
+    val port = "<$port>"
+    val username = "<$user>"
+    val password = "<$password>"
+    val mysql = MySQL(hostname, database, port, username, password)
+    mysql.connect().use { conn ->
+        println(String.format("Conected: %b", !conn.isClosed))
+        if (conn.isClosed) {
+            println("Terminating as the connection is closed.")
+            exitProcess(1)
+        }
+        conn.createStatement().executeQuery("SELECT 1+1").use {resultSet ->
+            if (resultSet.next()) {
+                println("Result of query ${resultSet.getInt("1+1")}: %s")
+            }
+        }
+    }
+}
